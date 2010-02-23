8fc880e707747be5208fd98607de76ad9013cfd3
thomas.wirum <thomas.wirum@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-02-23 23:36:16 +0000

    new bot project

---
+++ bot/src/bot/Main.java
@@ -0,0 +1,82 @@
+package bot;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		// The server to connect to and our details.
+		String server = "irc.homelien.no";
+		int port = 6667;
+		String nick = "zicbot";
+		String login = "zicbot";
+
+		// The channel which the Main will join.
+		String channel = "#zictest";
+
+		// Connect directly to the IRC server over socket, using buffered reads
+		// and writes.
+		Socket socket = new Socket(server, port);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				socket.getOutputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket
+				.getInputStream()));
+
+		// Log on to the server.
+		writer.write("NICK " + nick + "\r\n");
+		writer.write("USER " + login + " 8 * : Java IRC Bot Project\r\n");
+		writer.flush();
+
+		// Read lines from the server until it tells us we have connected.
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.indexOf("004") >= 0) {
+				// We are now logged in.
+				break;
+			} else if (line.indexOf("433") >= 0) {
+				System.out.println("Nickname is already in use.");
+				return;
+			}
+		}
+
+		// Join the channel.
+		writer.write("JOIN " + channel + "\r\n");
+		writer.flush();
+
+		// Keep reading lines from the server.
+		// We should call all functionality from this loop.
+		while ((line = reader.readLine()) != null) {
+			if (line.startsWith("PING ")) {
+				// We must respond to PINGs to avoid being disconnected.
+				writer.write("PONG " + line.substring(5) + "\r\n");
+				// writer.write("PRIVMSG " + channel + " :I got pinged!\r\n");
+				writer.flush();
+			} else {
+				// Print the raw line received by the Main.
+				System.out.println(line);
+			}
+			// Close the socket and break out of the loop if told so
+			// probably might want to comment this out in production ;)
+			//if (line.contains("PRIVMSG " + channel + " :!die")) {
+			//	socket.close();
+			//	break;
+			//}
+			if (line.contains("PRIVMSG " + channel + " :!weather")) {
+				XMLReader xml = new XMLReader();
+				String parm = line.substring(line.indexOf("weather") + 8)
+						.replaceAll(" ", "%20");
+				String[] result = xml.parseData(parm);
+				writer.write("PRIVMSG " + channel + " :Location: " + result[0]
+						+ " Temp: " + result[1] + "C" + ", " + result[2]
+						+ "\r\n");
+				writer.flush();
+			}
+		}
+	}
+
+}
---
+++ bot/src/bot/XMLReader.java
@@ -0,0 +1,75 @@
+package bot;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XMLReader {
+
+	public static final class SaxHandler extends DefaultHandler {
+		String cityData = "";
+		String tempData = "";
+		String windData = "";
+
+		public String getCityData() {
+			return cityData;
+		}
+
+		public String getTempData() {
+			return tempData;
+		}
+
+		public String getWindData() {
+			return windData;
+		}
+
+		@Override
+		public void startElement(String uri, String localName, String wName,
+				Attributes attrs) throws SAXException {
+
+			if (wName.equals("city")) {
+				// get the corresponding attribute from data=""
+				cityData = attrs.getValue("data");
+			}
+			if (wName.equals("temp_c")) {
+				// get the corresponding attribute from data=""
+				tempData = attrs.getValue("data");
+			}
+			if (wName.equals("wind_condition")) {
+				// get the corresponding attribute from data=""
+				windData = attrs.getValue("data");
+			}
+		}
+	}
+
+	public String[] parseData(String city) throws MalformedURLException {
+		URL url = new URL("http://www.google.com/ig/api?weather=" + city);
+		String[] resultArray = new String[3];
+		try {
+			// creates and returns new instance of SAX-implementation:
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+
+			// create SAX-parser...
+			SAXParser parser = factory.newSAXParser();
+			// .. define our handler:
+			SaxHandler handler = new SaxHandler();
+
+			// and parse:
+			parser.parse(url.openStream(), handler);
+
+			resultArray[0] = handler.getCityData();
+			resultArray[1] = handler.getTempData();
+			resultArray[2] = handler.getWindData();
+
+		} catch (Exception ex) {
+			ex.printStackTrace(System.out);
+		}
+		return resultArray;
+	}
+}
