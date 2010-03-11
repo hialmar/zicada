6b9d07429e6a86dfe8b7a8a266286e5ceef07373
thomas.wirum <thomas.wirum@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-03-11 16:37:11 +0000

    Some cleanup and varname refactoring

--- bot/src/bot/Main.java
+++ bot/src/bot/Main.java
@@ -11,21 +11,19 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		// The server to connect to and our details.
-		String server = "irc.sorcery.net";
+		String server = "irc.homelien.no";
 		int port = 6667;
 		String nick = "zicbot";
 		String login = "zicbot";
-
-		// The channel which the Main will join.
-		String channel = "#alfa";
+		String channel = "#zictest";
 
 		// Connect directly to the IRC server over socket, using buffered reads
 		// and writes.
 		Socket socket = new Socket(server, port);
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
 				socket.getOutputStream()));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(socket
-				.getInputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+                                socket.getInputStream()));
 
 		// Log on to the server.
 		writer.write("NICK " + nick + "\r\n");
@@ -54,23 +52,16 @@ public class Main {
 			if (line.startsWith("PING ")) {
 				// We must respond to PINGs to avoid being disconnected.
 				writer.write("PONG " + line.substring(5) + "\r\n");
-				// writer.write("PRIVMSG " + channel + " :I got pinged!\r\n");
 				writer.flush();
 			} else {
 				// Print the raw line received by the Main.
 				System.out.println(line);
 			}
-			// Close the socket and break out of the loop if told so
-			// probably might want to comment this out in production ;)
-			//if (line.contains("PRIVMSG " + channel + " :!die")) { 
-			//	socket.close();
-			//	break;
-			//}
 			if (line.contains("PRIVMSG " + channel + " :!weather")) {
 				XMLReader xml = new XMLReader();
-				String parm = line.substring(line.indexOf("weather") + 8)
+				String location = line.substring(line.indexOf("weather") + 8)
 						.replaceAll(" ", "%20");
-				String[] result = xml.parseData(parm);
+				String[] result = xml.parseData(location);
 				writer.write("PRIVMSG " + channel + " :Location: " + result[0]
 						+ ", Temp: " + result[1] + "C" + ", " + result[2]
                                                 + " " + result[3] + "\r\n");
@@ -78,5 +69,4 @@ public class Main {
 			}
 		}
 	}
-
 }
