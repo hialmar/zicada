ef8355bc3b835651493b0409328d3fb25798a40e
benny2k <benny2k@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-03-15 16:43:46 +0000

    My dice constructor class.

---
+++ bot/src/bot/Dice.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bot;
+
+/**
+ *
+ * @author b9
+ */
+
+import java.util.Random;
+
+
+
+public class Dice {
+
+
+    private Random generator;
+    private int randlim;
+
+    public Dice() {
+
+
+        generator = new Random();
+
+
+    }
+
+   public int rand(int a){
+
+
+
+
+            randlim=  generator.nextInt(a);
+
+            return randlim + 1;
+
+
+    }
+
+}
