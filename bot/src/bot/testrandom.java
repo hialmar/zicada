04c563e47d1d3c84986d306721545ac7bc9793c1
benny2k <benny2k@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-03-23 10:39:47 +0000

    Updated the code, made some major improvements. Instead of priting out the dice roll every time the loop repeats it now appends it all to a string which it prints after the loop is done.
    Also managed to get the code to take an input like !roll d3d4+5 (!roll dXdY+Z)

--- bot/src/bot/testrandom.java
+++ bot/src/bot/testrandom.java
@@ -5,7 +5,6 @@
 package bot;
 
 import javax.swing.JOptionPane;
-import bot.Dice;
 
 /**
  *
@@ -14,25 +13,44 @@ import bot.Dice;
 public class testrandom {
 
     public static void main(String[] args) {
-        // Dette er bare en tidlig prototype.
-
-        String test = JOptionPane.showInputDialog("Terning øyne");
-
-        String test2 = JOptionPane.showInputDialog("Terningkast");
 
 
-//String test3 = JOptionPane.showInputDialog("bonus");
+        // example of what the input from IRC would look like
+        String input = "!roll d6d6+3";
+
+        
+
+        // fetching the parts wanted from the input
+       
+        // Number of eyes on the dice
+        String dProp1 = input.substring(7,8);
+        // Number of time the dice is rolled
+        String dProp2 = input.substring(9,10);
+        // Dice bonus. If it is +2, then when the scrip rolls 4, it will come out as 6.
+        String dProp3 = input.substring(11,12);
 
 
-        int a = Integer.parseInt(test);
-        int b = Integer.parseInt(test2);
+        // Parsing the strings.
+        int a = Integer.parseInt(dProp1);
+        int b = Integer.parseInt(dProp2);
+        int x = Integer.parseInt(dProp3);
         int c = 1;
+
+        // Rolling the dice, then appending the results to the result string.
+       String result = "";
         for (int i = b; i > 0; i--) {
 
             Dice rand = new Dice();
-            System.out.print("Roll " + c + ": " + rand.rand(a) + "\n");
+
+
+            String lol =  ("Roll " + c + ": " + rand.rand(a, 0) + "\n");
             c++;
 
+            result += lol;
+
         }
+
+       // Prints out what has been appended to result.
+       System.out.println(result);
     }
 }
