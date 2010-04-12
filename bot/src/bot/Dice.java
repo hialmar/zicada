35ae96c5d7906061fbf35953c361e1ce22963107
benny2k <benny2k@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-04-12 04:46:07 +0000

    Fixed a small bug where the result would look like this: Roll 1: 5 Roll 1: 7 Roll 1: 2

--- bot/src/bot/Dice.java
+++ bot/src/bot/Dice.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package bot;
 
-/**
- *
- * @author b9
- */
 import java.util.Random;
 
 public class Dice {
@@ -26,8 +18,8 @@ public class Dice {
 
     public String test(int a, int b, int c) {
         String result = "";
+        int x = 1;
         for (int i = b; i > 0; i--) {
-            int x = 1;
             Dice rand = new Dice();
             String lol = ("Roll " + x + ": " + rand.rand(a, c) + "\n");
             x++;
