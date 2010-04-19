1fee73cd1666e768bfd7c1f3082fa355ce02dfe4
benny2k <benny2k@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-04-19 20:57:34 +0000

    testrandom now takes the input XdY+/-Z.

--- bot/src/bot/testrandom.java
+++ bot/src/bot/testrandom.java
@@ -1,67 +1,50 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package bot;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class testrandom.
- *
- * @author b9
- */
 public class testrandom {
 
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// example of input. the + can also be -
-		String input = "!roll d5d6+2";
+        //example of input. the + can also be -
+        String input = "1d6+10";
+        System.out.println(input);
 
-		// checks wether the string contains "+" or "-" then changes it to a
-		// "d".
-		if (input.contains("+")) {
-			input = input.replaceAll("[+]", "d");
-			// splits the string at d's.
-			String[] res = input.split("d");
-			// Parsing the strings.
-			int a = Integer.parseInt(res[1]);
-			int b = Integer.parseInt(res[2]);
-			int c = Integer.parseInt(res[3]);
-			// creating dice object
-			Dice diceObject = new Dice();
-			// runs the method using the user input and returns
-			// the result to the string named result.
-			String result = diceObject.test(a, b, c);
-			System.out.println(result);
+        //checks wether the string contains "+" or "-" then changes it to a "d".
+        if (input.contains("+")) {
+            input = input.replaceAll("[+]", "d");
+            //splits the string at d's.
+            String[] res = input.split("d");
+            // Parsing the strings.
+            int a = Integer.parseInt(res[0]);
+            int b = Integer.parseInt(res[1]);
+            int c = Integer.parseInt(res[2]);
+            //creating dice object
+            Dice diceObject = new Dice();
+            //runs the method using the user input and returns
+            //the result to the string named result.
+            int result = diceObject.test(a, b, c);
+            System.out.println(input + " is " + result);
 
-			// same as the if test above, but in this instance the string
-			// contains a "-".
-		} else if (input.contains("-")) {
-			input = input.replaceAll("-", "d-");
-			String[] res = input.split("d");
-			int a = Integer.parseInt(res[1]);
-			int b = Integer.parseInt(res[2]);
-			int c = Integer.parseInt(res[3]);
-			Dice diceObject = new Dice();
-			String result = diceObject.test(a, b, c);
-			System.out.println(result);
+            //same as the if test above, but in this instance the string contains a "-".
+        } else if (input.contains("-")) {
+            input = input.replaceAll("-", "d-");
+            String[] res = input.split("d");
+            int a = Integer.parseInt(res[0]);
+            int b = Integer.parseInt(res[1]);
+            int c = Integer.parseInt(res[2]);
+            Dice diceObject = new Dice();
+            int result = diceObject.test(a, b, c);
+            System.out.println(input + " is " + result);
 
-			// this code is run in cases where there is no "+" or "-" in the
-			// input.
-		} else {
-			String[] res = input.split("d");
-			int a = Integer.parseInt(res[1]);
-			int b = Integer.parseInt(res[2]);
-			int c = 0;
-			Dice diceObject = new Dice();
-			String result = diceObject.test(a, b, c);
-			System.out.println(result);
-		}
+            //this code is run in cases where there is no "+" or "-" in the input.
+        } else {
+            String[] res = input.split("d");
+            int a = Integer.parseInt(res[0]);
+            int b = Integer.parseInt(res[1]);
+            int c = 0;
+            Dice diceObject = new Dice();
+            int result = diceObject.test(a, b, c);
+            System.out.println(input + " is " + result);
+        }
 
-	}
+    }
 }
