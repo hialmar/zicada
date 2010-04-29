7e9a05037da5aa4fdcbd0429680747ed4f884ba6
thomas.wirum <thomas.wirum@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-04-29 19:40:11 +0000

    Added GPLv3 header to every file

--- bot/src/bot/Dice.java
+++ bot/src/bot/Dice.java
@@ -1,3 +1,20 @@
+/*
+ *     This file is part of zicbot.
+
+    zicbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    zicbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with zicbot.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 package bot;
 
 import java.util.Random;
@@ -107,7 +124,7 @@ public class Dice {
             /*Runs the function which rolls the dice,
             using the numbers fetched from input */
             int result = roll(x, y, z);
-            String diceResult = ("you rolled " + query + ". And got: " + result);
+            String diceResult = (query + ": " + result);
             return diceResult;
 
         }
--- bot/src/bot/HtmlParser.java
+++ bot/src/bot/HtmlParser.java
@@ -1,3 +1,20 @@
+/*
+ *     This file is part of zicbot.
+
+    zicbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    zicbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with zicbot.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 package bot;
 
 import java.io.IOException;
@@ -6,6 +23,9 @@ import java.net.URL;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Source;
 
+/**
+ * Class for parsing html from the various websites used by the bot.
+ */
 public class HtmlParser {
 
 	private String name;
@@ -14,7 +34,7 @@ public class HtmlParser {
 	private Source source;
 
 	/**
-	 * Gets the tlf data.
+	 * Gets data from http://www.telefonkatalogen.no/
 	 * 
 	 * @param query
 	 *            the query
