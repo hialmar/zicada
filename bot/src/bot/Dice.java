a6dc07e6c2c582be10f44ba30bb6e5bc68c463e1
benny2k <benny2k@ab96be5e-20c7-11df-b311-37335b2923d9>
2010-04-29 18:50:16 +0000

    Second attempt at fixing the bug where the !roll command wouldn't take a XdY-Z argument.

--- bot/src/bot/Dice.java
+++ bot/src/bot/Dice.java
@@ -7,72 +7,70 @@ import java.util.Random;
  */
 public class Dice {
 
-	/** The generator. */
+    /** The generator. */
     private Random generator;
+    /** The randlim. */
+    private int randlim;
 
-	/** The randlim. */
- 	private int randlim;
-
-	/**
-	 * Instantiates a new dice.
-	 */
+    /**
+     * Instantiates a new dice.
+     */
     public Dice() {
         generator = new Random();
     }
 
-	/**
-	 * Rand.
-	 *
-	 * @param a the a
-	 * @return the int
-	 */
+    /**
+     * Rand.
+     *
+     * @param a the a
+     * @return the int
+     */
     public int rand(int a) {
         randlim = generator.nextInt(a);
         return randlim + 1;
     }
 
-	/**
-	 * Test.
-	 *
-	 * @param a the a
-	 * @param b the b
-	 * @param c the c
-	 * @return the string
-	 */
+    /**
+     * Test.
+     *
+     * @param a the a
+     * @param b the b
+     * @param c the c
+     * @return the string
+     */
     public int roll(int a, int b, int c) {
         int result = 0;
         for (int i = a; i > 0; i--) {
             Dice rand = new Dice();
             int getDice = (rand.rand(b));
-           
 
-                result += getDice;
+            result += getDice;
 
-        result += c;
         }
+        result += c;
         return result;
     }
 
-    public String[] output (String input) {
+    public String[] output(String input) {
 
         String res[] = null;
 
-        if (!input.contains("+")) {
+        if (!input.contains("+") && !input.contains("-")) {
             input += "+0";
+
         }
-        if (input.contains("+")) {
-            input = input.replaceAll("[+]","d");
+        if (input.contains("-")) {
+            input = input.replaceAll("[-]", "d-");
 
         } else {
-            input = input.replaceAll("-", "d-");
+            input = input.replaceAll("[+]", "d");
         }
 
         res = input.split("d");
-
         return res;
     }
 
-    public String rollPrint (String query) {
+    public String rollPrint(String query) {
 
         if (query.isEmpty()) {
 
@@ -80,16 +78,16 @@ public class Dice {
 
         } else {
             String res[] = output(query);
-        /*Parses the array to integers*/
-        int x = Integer.parseInt(res[0]);
-        int y = Integer.parseInt(res[1]);
-        int z = Integer.parseInt(res[2]);
-        /*Runs the function which rolls the dice,
-        using the numbers fetched from input */
-        int result = roll(x, y, z);
-        String diceResult = ("you rolled " + query + ". And got: " + result);
-        return diceResult;
+            /*Parses the array to integers*/
+            int x = Integer.parseInt(res[0]);
+            int y = Integer.parseInt(res[1]);
+            int z = Integer.parseInt(res[2]);
+            /*Runs the function which rolls the dice,
+            using the numbers fetched from input */
+            int result = roll(x, y, z);
+            String diceResult = ("you rolled " + query + ". And got: " + result);
+            return diceResult;
 
-       }
+        }
     }
 }
